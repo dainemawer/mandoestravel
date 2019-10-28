39d57c0a76ac73b17157294603b66d0ac59bdc5c
Daine Mawer <hello@dainemawer.com>
2019-10-28 20:57:30 +0200

    :postbox: Added Styled Components, Pages and Tweaked CSS

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const PrivacyPage = () => (
+    <Layout>
+        <SEO title="Privacy Policy" />
+        <div>Privacy Policy</div>
+    </Layout>
+)
+
+export default PrivacyPage
